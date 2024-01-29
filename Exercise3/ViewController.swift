6499f4165e0e48c2f2ec3f8117a71d7fba6f9aee
Madeshvinayagam <madeshv@yahoo.com>
2024-01-28 21:38:00 -0800

    written code for Add, Reset buttons

--- Exercise3/ViewController.swift
+++ Exercise3/ViewController.swift
@@ -15,9 +15,27 @@ class ViewController: UIViewController {
     }
 
     
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var countryTextField: UITextField!
+    
+    
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    
+    @IBOutlet weak var textViewInput: UITextView!
+    
+    
+    @IBOutlet weak var invisibleLabel: UILabel!
+    
+    
+    
     @IBAction func firstNameInput(_ sender: Any) {
     }
-    
     
     @IBAction func lastNameInput(_ sender: Any) {
     }
@@ -30,22 +48,51 @@ class ViewController: UIViewController {
     }
     
     
-    @IBOutlet weak var textViewInput: UITextView!
-    
-    
-    @IBOutlet weak var invisibleLabel: UILabel!
-    
-    
     @IBAction func addButton(_ sender: Any) {
+        updateTextView()
     }
     
     
-    @IBAction func submitButton(_ sender: Any) {
+    @IBAction func submitbutton(_ sender: Any) {
+        if isInformationComplete() {
+                    invisibleLabel.text = "Successfully submitted!"
+                } else {
+                    invisibleLabel.text = "Complete the missing Info!"
+                }
     }
     
     
     @IBAction func clearButton(_ sender: Any) {
+        clearAllFields()
     }
+    
+        private func updateTextView() {
+            let userInfo = """
+            First Name: \(firstNameTextField.text ?? "")
+            Last Name: \(lastNameTextField.text ?? "")
+            Country: \(countryTextField.text ?? "")
+            Age: \(ageTextField.text ?? "")
+            """
+            textViewInput.text = userInfo
+        }
+    
+    
+    
+        private func isInformationComplete() -> Bool {
+            return !firstNameTextField.text!.isEmpty &&
+                   !lastNameTextField.text!.isEmpty &&
+                   !countryTextField.text!.isEmpty &&
+                   !ageTextField.text!.isEmpty
+        }
+    
+        private func clearAllFields() {
+            firstNameTextField.text = ""
+            lastNameTextField.text = ""
+            countryTextField.text = ""
+            ageTextField.text = ""
+            textViewInput.text = ""
+            invisibleLabel.text = ""
+        }
     
 }
 
