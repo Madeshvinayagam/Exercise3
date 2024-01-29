642b1b343c0c314c0c751ed4951e0dc514ae30d9
Madeshvinayagam <madeshv@yahoo.com>
2024-01-28 20:34:49 -0800

    UI design completed

--- Exercise3/ViewController.swift
+++ Exercise3/ViewController.swift
@@ -14,6 +14,38 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func firstNameInput(_ sender: Any) {
+    }
+    
+    
+    @IBAction func lastNameInput(_ sender: Any) {
+    }
+    
+    
+    @IBAction func countryInput(_ sender: Any) {
+    }
+    
+    @IBAction func ageInput(_ sender: Any) {
+    }
+    
+    
+    @IBOutlet weak var textViewInput: UITextView!
+    
+    
+    @IBOutlet weak var invisibleLabel: UILabel!
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func submitButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func clearButton(_ sender: Any) {
+    }
+    
 }
 
